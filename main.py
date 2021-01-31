372acaa2b99632142c403238f879e230b38916cc
obey <georgecarlin770@gmail.com>
2021-01-31 14:04:05 +0300

    added velocity and delay logic\nautojump doesn't work yet WIP

--- main.py
+++ main.py
@@ -6,18 +6,26 @@ from copy import deepcopy
 
 os.environ["DISPLAY"] = ":0"
 
-
+x1, x2 = 500,600
 
 COORDS = [
-            (285,360)
+            (x1, 360),
+            (x2, 360)
          ]
+DISTANCE = x2-x1
+DINO = 260
+
 
 def main():
+    queue = []
     while True:
-        t1 = time.perf_counter()
-        pixel = ImageGrab.grab().getpixel(COORDS[0])
-        if sum(pixel)/3 < 170:
-            keyboard.send('space')
-        print(1/(time.perf_counter()-t1), end='\r')
-        
+        frame = ImageGrab.grab()
+        pixel1 = frame.getpixel(COORDS[0])
+        pixel2 = frame.getpixel(COORDS[1])
+        if sum(pixel2)/3 < 170:
+            queue.append(time.perf_counter())
+        if sum(pixel1)/3 < 170:
+            velocity = DISTANCE/(time.perf_counter()-queue.pop(0))
+            print(velocity)
+            
 main()
