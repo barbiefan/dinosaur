9b705aff8083041cce406f03c0611cfdac163b89
obey <georgecarlin770@gmail.com>
2021-02-01 10:32:48 +0300

    refactoring changed decision making removed \"same cactus\" logic added ducking for birds moved to matrix checking completely

--- main.py
+++ main.py
@@ -2,78 +2,57 @@ from PIL import ImageGrab
 import time
 import keyboard
 import os
-from copy import deepcopy
-
-
+import numpy
 
 os.environ["DISPLAY"] = ":0"
 
-end = 340
-xs = list(range(290,end,1))
-COORDS = [
-            [x, 360] for x in xs
-         ]
-height_coords = [
-            (end, 340),
-            (end-2, 340),
-            (end-4, 340)
-         ]
-passed_coords = (200, 360)
+y1 = 330 #y2-y1=15
+y2 = 345
+y3 = 346 #y4-y3=19
+y4 = 365
+
+x1 = 300+20 #x2-x1=50
+x2 = 360+20
+
+upper_flag = []
+lower_flag = []
 
 DINO = 260
-print(COORDS)
-
 
 def main():
-    counter = 0
-    forward_queue = [0]
-    height_flag = False
+    upper_flag = False
+    lower_flag = False
     while True:
+        time.sleep(0.05)
+
         frame = ImageGrab.grab()
-        pixels = [frame.getpixel(tuple(coord)) for coord in COORDS]
-        height_pixels = [frame.getpixel(coord) for coord in height_coords]
-        #passed_pixel = frame.getpixel(passed_coords)
-        time_pos = time.perf_counter()
-        if (83,83,83) in height_pixels:
-            height_flag = True
-        if (83,83,83) in pixels:
-            timestamp = time_pos-forward_queue[-1]
-            if timestamp > 0.5:
-                jump(height_flag)
-                forward_queue.pop(0)
-                forward_queue.append(time_pos)
-                print("jumped", forward_queue)
-                height_flag = False
-        #if passed_pixel == (83,83,83):
-        #   print('ducked for 0.2')
-        #    duck(0.2)
+        pixels = numpy.array(frame)
+        upper_zone = pixels[y1:y2,x1:x2]
+        lower_zone = pixels[y3:y4,x1:x2]
 
+        if (83,83,83) in upper_zone:
+            upper_flag = True
+        if (83,83,83) in lower_zone:
+            lower_flag = True
+        action(upper_flag, lower_flag)
+        upper_flag, lower_flag = False, False
 
-        #elif height_flag:
-        #    duck()
-
-        '''
-        if sum(pixel1)/3 < 170:
-            timestamp = time.perf_counter()-queue[-1]
-            if timestamp > 0.5:
-                print("pixel1", queue)
-                velocity = DISTANCE/(time.perf_counter()-queue.pop(0))
-                print(velocity)
-        ''' 
-        counter += 0
-        if counter == 100:
-            counter = 0
-            xs.append(xs[-1]+2)
-            COORDS.append([xs[-1], 360])
-            print(COORDS)
-
+def action(u_flag, l_flag):
+    if u_flag:
+        if l_flag:
+            jump(True)
+        else:
+            duck(0.4)
+    elif l_flag:
+        jump(False)
+        
 def duck(sec):
     keyboard.press('down arrow')
     time.sleep(sec)
     keyboard.release('down arrow')
 
-def jump(flag):
-    if not flag:
+def jump(long):
+    if not long:
         keyboard.press('space')
         time.sleep(0.2)
         keyboard.release('space')
@@ -87,3 +66,9 @@ def jump(flag):
         print('long jumped')
 
 main()
+
+'''
+if up and low: jump long
+if up but not low: duck
+if not up but low: jump short
+'''
